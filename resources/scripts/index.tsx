f388dd6ecea589932352051ccfe0251a7bbb3712
KarutoKoshiza <karutokoshiza@gmail.com>
2024-03-14 10:06:08 +0700

    delete cuy

--- resources/scripts/index.tsx
+++ resources/scripts/index.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("app")!).render(
     <>
-        <p>Hello World cuy </p>
+        <p>Hello World </p>
     </>
 )
