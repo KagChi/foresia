77926e275e7567b0a5ae3ff3552b1e492536cabe
KagChi <yonuxbot@gmail.com>
2024-05-01 22:48:24 +0700

    fix: use xata database

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,9 +1,7 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
-import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.DATABASE_URL!);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 10 });
 
-const db = drizzle(sql, { schema });
-
-export default db;
+export default drizzle(pool, { schema });
