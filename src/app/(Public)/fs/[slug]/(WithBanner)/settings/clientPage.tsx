1c7a9eeb0cb02af7a674f30aac80a10c786d7457
KagChi <yonuxbot@gmail.com>
2024-05-02 09:57:38 +0700

    chore: update settings form

--- src/app/(Public)/fs/[slug]/(WithBanner)/settings/clientPage.tsx
+++ src/app/(Public)/fs/[slug]/(WithBanner)/settings/clientPage.tsx
@@ -13,13 +13,24 @@ export default function ClientPage() {
     return (
         <>
             <div className="px-6 py-4">
-                <form>
+                <form className="flex flex-col gap-2">
                     <div className="flex flex-col gap-2 text-white">
-                        <p className="text-2xl font-semibold">Description<span className="text-red-500">*</span></p>
+                        <p className="text-xl font-semibold">Name<span className="text-red-500">*</span></p>
+                        <input name="description" required className="min-h-8 w-full rounded-md bg-[#1B1B1B] px-4 py-2 outline-none" />
+                    </div>
+                    <div className="flex flex-col gap-2 text-white">
+                        <p className="text-xl font-semibold">Description<span className="text-red-500">*</span></p>
                         <textarea name="description" required className="max-h-72 min-h-72 w-full rounded-md bg-[#1B1B1B] px-4 py-2 outline-none" />
                     </div>
 
-                    <SubmitButton.Primary text="Save changes" />
+                    <div className="flex flex-col gap-2 text-white">
+                        <p className="text-xl font-semibold">Rules<span className="text-red-500">*</span></p>
+                        <input id="rule" type="text" className="min-h-8 w-full rounded-md bg-[#1B1B1B] px-4 py-2 outline-none" />
+                    </div>
+
+                    <div>
+                        <SubmitButton.Primary text="Save changes" />
+                    </div>
                 </form>
 
                 <form action={() => {
