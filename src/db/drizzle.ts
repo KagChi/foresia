e6be40b4585d270e95e25cb0f4046e6509b333f6
KagChi <yonuxbot@gmail.com>
2024-05-02 18:47:19 +0700

    chore: bump connection limit

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,6 +2,6 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 10 });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 36 });
 
 export default drizzle(pool, { schema });
