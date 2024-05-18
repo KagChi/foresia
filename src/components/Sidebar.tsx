e32cb57eab83b1611db7da1f087e48d220e04ea5
KagChi <yonuxbot@gmail.com>
2024-05-18 09:58:55 +0700

    chore: hide route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -59,7 +59,7 @@ export const Sidebar = ({ children, profile }: Readonly<{ children?: React.React
                         </p>
                         <div className="flex flex-col font-medium">
                             <a href="/create/community" className="font-medium text-gray-400">Create new community</a>
-                            <a href="/popular" className="font-medium text-gray-400">Popular Subrealms</a>
+                            {/* <a href="/popular" className="font-medium text-gray-400">Popular Subrealms</a> */}
                         </div>
                     </div>
 
